6154642274657ef0866591a045108fd1fe597790
Bogdan Gaevskij <liltracyn@yandex.ru>
2024-12-06 21:25:05 +0300

    first-commit

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import classes from './App.module.scss';
+
+export const App = () => {
+  const [count, setCount] = useState<number>(0);
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment} className={classes.button}>inc</button>
+      <button onClick={decrement}>
+        dec
+      </button>
+    </div>
+  );
+};
